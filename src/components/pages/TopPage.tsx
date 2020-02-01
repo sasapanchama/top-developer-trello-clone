427ef246669ef3359c3b3c3670c383dd04e412f2
eishis-sasai <eishis-sasai@eishis-sasai-mac.local>
2020-02-01 11:23:12 +0900

    ADD TopPage's style

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,10 +1,60 @@
 import React, { FC } from 'react';
+import '../../styles/pages/Base.scss';
 
 type Props = { }
 
 const TopPage: FC<Props> = () => {
   return (
-    <div>TopPage</div>
+    <>
+    <header className="Header"></header>
+    <ul className="Boards">
+      <li className="Board">
+        <a href="#">
+          BoardTitle
+        </a>
+      </li>
+      <li className="Board">
+        <a href="#">
+          BoardTitle
+        </a>
+      </li>
+      <li className="Board">
+        <a href="#">
+          BoardTitle
+        </a>
+      </li>
+      <li className="Board">
+        <a href="#">
+          BoardTitle
+        </a>
+      </li>
+      <li className="Board">
+        <a href="#">
+          BoardTitle
+        </a>
+      </li>
+      <li className="Board">
+        <a href="#">
+          BoardTitle
+        </a>
+      </li>
+      <li className="Board BoardCreate">
+        <button className="BoardCreate_Label">
+          Create New Board!
+        </button>
+        {/* <div className="BoardCreate_Box">
+          <input className="Field FieldText" type="text" />
+          <div className="BoardCreate_Box_Button">
+            <button className="Button ButtonCancel">CANCEL</button>
+            <button className="Button ButtonCreate">CREATE</button>
+          </div>
+        </div> */}
+      </li>
+    </ul>
+    <footer className="Footer">
+      <p className="Footer_Copyright">&copy;copyright.</p>
+    </footer>
+    </>
   );
 };
 
