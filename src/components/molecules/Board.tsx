610f04d281d3f5cb85a247ddc58d03219d2e94b3
eishis-sasai <eishis-sasai@eishis-sasai-mac.local>
2020-02-02 00:05:52 +0900

    SORT routing

--- src/components/molecules/Board.tsx
+++ src/components/molecules/Board.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 import '../../styles/molecules/Board.scss';
 
 type Props = { text: string };
@@ -6,9 +7,8 @@ type Props = { text: string };
 const Board: FC<Props> = ({ text }) => {
   return (
     <li className="Board">
-      <a href="#">
-        {text}
-      </a>
+      <Link to={{ pathname: `/bottom` }}>{text}</Link>
+      {/* <Link to={{ pathname: `/${user}/${board}`, state: { index } }}>{text}</Link> */}
     </li>
   );
 };
